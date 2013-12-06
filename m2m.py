6ad87d883f538a405fa9ec16f8562e599d90a333
dreispt <dreis.pt@gmail.com>
2013-12-06 14:07:13 +0000

    Improved docs

--- m2m.py
+++ m2m.py
@@ -1,16 +1,30 @@
 """
-Wrapper for OpenERP's cryptic write conventions for x2many fields.
+Wrapper for OpenERP's cryptic for x2many fields writing conventions.
+
 Example usage:
 
     import m2m
-    browse_rec.write({'many_ids: m2m.clear())
-    browse_rec.write({'many_ids: m2m.link(99))
-    browse_rec.write({'many_ids: m2m.add({'name': 'Monty'}))
-    browse_rec.write({'many_ids: m2m.replace([98, 99]))
+    browse_rec.write({'many_ids': m2m.clear())
+    browse_rec.write({'many_ids': m2m.link(99))
+    browse_rec.write({'many_ids': m2m.add({'name': 'Monty'}))
+    browse_rec.write({'many_ids': m2m.replace([98, 99]))
 
 Since returned values are lists, the can be joined using the plus operator:
 
-    browse_rec.write({'many_ids: m2m.clear() + m2m.link(99))
+    browse_rec.write({'many_ids': m2m.clear() + m2m.link(99))
+
+Operations supported on one2many and many2many:
+
+  * `create(values)` or `add(values)`: create and link new record
+  * `write(id, values)`: update a currently linked record
+  * `remove(id)`: unlink and delete the referenced record
+
+Operations supported on many2many fields only:
+
+  * `unlink(id)`: unlink the record without deleting it
+  * `link(id)`: add a link to an existing record
+  * `clear()`: unlink all references
+  * `replace(ids)`: unlink all references and replace them by an id list
 
 """
 
@@ -46,7 +60,7 @@ def unlink(id):
 
 
 def link(id):
-    """ Link but do not delete the referenced record """
+    """ Add a link to the existing record """
     assert isinstance(id, int)
     return [(4, id)]
 
